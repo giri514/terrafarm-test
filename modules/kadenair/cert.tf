72c9e535d479a5ba9aea8977a8bab33ac2722c44
giri514 <90270592+giri514@users.noreply.github.com>
2024-01-12 17:55:12 -0600

    Update cert.tf

--- modules/kadenair/cert.tf
+++ modules/kadenair/cert.tf
@@ -1,8 +1,16 @@
 # Configure the AWS Provider
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
 provider "aws" {
   region = "us-east-1"
- }
-
+}
 resource "aws_acm_certificate" "ssl_certificate" {
   domain_name       = var.certificatedomain
   subject_alternative_names = ["www.${var.certificatedomain}"]
